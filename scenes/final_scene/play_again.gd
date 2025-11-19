573b07e9ed21c5bc9f1bc0353d03655fe4e64deb
ivdobry <ivan@rzilient.club>
2025-11-19 19:24:43 +0100

    Create final scene with exit button.

---
+++ scenes/final_scene/play_again.gd
@@ -0,0 +1,11 @@
+extends Button
+
+@export var main_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pressed.connect(_play_again, 4)
+
+
+func _play_again() -> void:
+	get_tree().change_scene_to_packed(main_scene)
